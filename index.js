131c16fe672ec81f350d659e93fd9465e78110a0
techlover <e.rono@outlook.com>
2023-03-31 22:18:22 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Karen';
+
+function changeLeastFavoriteCustomer() {
+  // This will throw a TypeError, since you can't reassign a const
+  leastFavoriteCustomer = 'Susan';
+}
